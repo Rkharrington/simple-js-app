de856daeffd23a2587d111e2374eaa82ad312ac7
Rkharrington <rachel.harrington2@gmail.com>
2023-10-13 18:37:37 -0500

    Update title, add heading

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    { name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    { name: 'Weedle', height: 0.3, type: ['bug', 'posion']}
+    { 
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    { 
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+    { 
+        name: 'Weedle',
+        height: 0.3,
+        type: ['bug', 'poison']
+    },
+    { 
+        name: 'Pidgetto',
+        height: 1.1,
+        type: ['flying', 'normal']
+    },
+    { name: 'Ekans',
+    height: 2,
+    type: ['poison']
+}
 ];
 
-let pokemon = ['Bulbasaur', 'Charizard', 'Weedle',];
+let pokemon = ['Bulbasaur', 'Charizard', 'Weedle', 'Pidgetto', 'Ekans'];
 
 for(let i = 0; i < pokemon.length; i++) {
-    console.log(pokemon[i]);
+    console.log(pokemon[i])
 }
