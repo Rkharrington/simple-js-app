1064cd3df75394830085a85a8573079e698568de
Rkharrington <rachel.harrington2@gmail.com>
2023-10-13 17:14:26 -0500

    Create for loop that iterates over each item in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    { name: 'Charmander', height: 0.6, type: ['fire']},
-    { name: 'Squirtle', height: 0.5, type: ['water']},
-    { name: 'Caterpie', height: 0.3, type: ['bug']},
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
     { name: 'Weedle', height: 0.3, type: ['bug', 'posion']}
 ];
+
+let pokemon = ['Bulbasaur', 'Charizard', 'Weedle',];
+
+for(let i = 0; i < pokemon.length; i++) {
+    console.log(pokemon[i]);
+}
