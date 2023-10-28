5c870f977093bfaf000101e3e484fddcfbec704a
Rkharrington <rachel.harrington2@gmail.com>
2023-10-27 22:20:26 -0500

    Wrap pokemonList in an IIFE, create pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,46 @@
-let pokemonList = [
-    { 
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['grass', ' poison']
-    },
-    { 
-        name: 'Charizard',
-        height: 1.7,
-        type: ['fire', ' flying']
-    },
-    { 
-        name: 'Weedle',
-        height: 0.3,
-        type: ['bug', ' poison']
-    },
-    { 
-        name: 'Pidgetto',
-        height: 1.1,
-        type: ['flying', ' normal']
-    },
-    { name: 'Ekans',
-    height: 2,
-    type: ['poison']
-    }
-];
-
-function myLoopFunction(pokemonList) {
-    document.write('<p>' + pokemonList.name + ' (height: ' + pokemonList.height + ' type: ' + pokemonList.type + ')' + '</p>');
-  }
-  pokemonList.forEach (myLoopFunction);
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { 
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ['grass', ' poison']
+        },
+        { 
+            name: 'Charizard',
+            height: 1.7,
+            type: ['fire', ' flying']
+        },
+        { 
+            name: 'Weedle',
+            height: 0.3,
+            type: ['bug', ' poison']
+        },
+        { 
+            name: 'Pidgetto',
+            height: 1.1,
+            type: ['flying', ' normal']
+        },
+        { 
+            name: 'Ekans',
+            height: 2,
+            type: ['poison']
+        }
+    ];
+    function getAll () {
+        return pokemonList;
+      }
+      function add (pokemon) {
+        pokemonList.push(pokemon);
+      }
+    
+      return {
+        getAll: getAll,
+        add: add
+      }
+      
+    })();
+    
+    document.write('');
+    pokemonRepository.getAll().forEach(function (pokemon) {
+      document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + ' type: ' + pokemon.type + ')' + '</p>');
+    });
