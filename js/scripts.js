bddc3dbfbe4014706e92656353c5a1b398fa58cb
Rkharrington <rachel.harrington2@gmail.com>
2023-10-30 17:56:50 -0500

    Practice DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -26,23 +26,30 @@ let pokemonRepository = (function () {
         }
     ];
 
-return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+      }
+  
+    function getAll() {
       return pokemonList;
     }
-  }
-})();
-
-document.write('');
-pokemonRepository.getAll().forEach(function(pokemon) {
-  let pokemonList = document.querySelector('.pokemon-list');
-  let listItem = document.createElement('li');
-  let button = document.createElement('button');
-  button.innerText = 'placeholder';
-  button.classList.add('button-class');
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-});
+    function addListItem(pokemon){
+      let pokemonList = document.querySelector('.pokemon-list');
+      let listpokemon = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+    }
+  
+    return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem
+    };
+  })();
+  
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
